06ccc17da0826a10719c2d9c6d0adcdb2904b4d6
Björn Hegerfors <Bj0rnen@hotmail.com>
2018-10-24 00:36:02 +0200

    Add generics-sop dependency

--- shell.nix
+++ shell.nix
@@ -1,8 +1,8 @@
 with (import <nixpkgs> {}).pkgs;
 let
-  ghcPackages = haskell.packages.ghc843;
+  ghcPackages = haskell.packages.ghc844;
   ghc = ghcPackages.ghcWithPackages
-          (pkgs: with pkgs; [ singletons constraints exinst ]);
+          (pkgs: with pkgs; [ singletons exinst constraints generics-sop ]);
 in
 stdenv.mkDerivation {
   name = "my-haskell-env-0";
